9356a51c4cb59a3c769e09a049b600be1f177f1e
Luis Martinez <luis.martinez@cidecuc.mx>
2023-09-07 16:06:43 -0600

    (feat): Delete successfuly

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -3,10 +3,22 @@ import { TaskModel } from "../models/TaskModel.js";
 
 
 const getProjects = async(req, res)=>{
-
+    try {
+        const data = await ProjectModel.findAll();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(404).send('error');
+        return
+    }
 };
 
 const getProject = async(req, res)=>{
+    try {
+        const data = '';
+    } catch (error) {
+        res.status(404).send('error');
+        return
+    }
 
 };
 
@@ -35,11 +47,38 @@ const createProject = async(req, res)=>{
 };
 
 const updateProject = async(req, res)=>{
+    try {
+        const {id} = req.params;
+
+        if(!id){
+            return res.send('id is valid');
+        }
+
+
+    } catch (error) {
+        res.status(404).send('error');
+        return
+    }
 
 };
 
 const deleteProject = async(req, res)=>{
+    try {
+        const {id} = req.params;
 
+        if(!id){
+            return res.send('id is valid');
+        }
+        const data = await ProjectModel.destroy({where:{id}});
+        if(!data){
+            return res.send('the project does not exist')
+        }
+        res.status(200).json({success:'Project deleted successfully'})
+        
+    } catch (error) {
+        res.status(404).send('error');
+        return
+    }
 };
 
 export {getProject, getProjects, createProject, updateProject, deleteProject}
